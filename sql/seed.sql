1bd185f622f3a329768c5f7e4adbd17a106a5443
carolpang <carolpang17@gmail.com>
2022-03-15 13:18:59 -0700

    department table datas created

---
+++ sql/seed.sql
@@ -0,0 +1,10 @@
+USE employeesDB;
+
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("Engineering");
+INSERT INTO department (name)
+VALUES ("HR");
+INSERT INTO department (name)
+VALUES ("Logistics");
