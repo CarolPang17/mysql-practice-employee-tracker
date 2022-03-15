2e923b9ab93450214fea072afee956f55a5aed86
carolpang <carolpang17@gmail.com>
2022-03-15 13:31:15 -0700

    roles data added

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,10 +1,20 @@
-USE employeesDB;
+USE employees_db;
 
 INSERT INTO department (name)
-VALUES ("Sales");
-INSERT INTO department (name)
-VALUES ("Engineering");
-INSERT INTO department (name)
-VALUES ("HR");
-INSERT INTO department (name)
-VALUES ("Logistics");
+VALUES
+("Sales"),
+("Engineering"),
+('Human Resources'),
+("Logistics");
+
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Lead Engineer', 190000, 2),
+('Software Engineer', 120000, 2),
+('Sales Rep', 50000, 1),
+('Logistics Manager', 170000, 4),
+('HR Staff', 60000, 3),
+('Sales Manager', 140000, 1),
+('import Staff', 70000, 4);
+
